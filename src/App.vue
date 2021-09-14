3cdb05eb254720640a0a3789cf9cc3843bb5c192
Bernard Thompson <bethomps@redhat.com>
2021-09-14 14:29:04 +0100

    using array of items for todos

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
-    <to-do-item label="My ToDo Item" v-bind:done="true"></to-do-item>
+      <ul>
+        <li v-for="item in ToDoItems" :key="item.id">
+          <to-do-item :label="item.label" v-bind:done="item.done" :id="item.id"></to-do-item>
+        </li>
+      </ul>
   </div>
 </template>
 
 <script>
 import ToDoItem from './components/ToDoItem.vue';
+import uniqueId from 'lodash.uniqueid';
 
 export default {
   name: 'app',
   components: {
     ToDoItem
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+        { id: uniqueId('todo-'), label: 'Have fun', done: true },
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
+      ]
+    };
   }
 };
 </script>
